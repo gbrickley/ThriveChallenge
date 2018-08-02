4867868ee95f44e97c451c470d8e539605f5381e
George <gbrickley88@gmail.com>
2018-08-02 15:47:42 -0700

    Initial Commit

---
+++ ThriveChallenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ThriveChallenge
+//
+//  Created by George Brickley on 8/2/18.
+//  Copyright © 2018 George Brickley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
